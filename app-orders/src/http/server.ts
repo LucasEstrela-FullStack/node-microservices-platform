3472da9add0798b30186c07c39a3a193d8259ee6
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2025-12-15 02:24:51 -0400

    feat: create orders and invoices services

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,12 +1,19 @@
-import { fastify } from "fastify";
+import { fastify } from "fastify"
+import { randomUUID } from "node:crypto"
 import { fastifyCors } from "@fastify/cors"
-import { z } from "zod";
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod"
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider} from "fastify-type-provider-zod"
+import { channels } from "../broker/channels/index.ts"
+import { schema } from "../db/schema/index.ts"
+import { db } from "../db/client.ts"
+import { dispatchOrderCreated } from "../broker/messages/order-created.ts"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, { origin: "*" })
 
 app.get("/health", () => {
   return "OK"
@@ -15,17 +22,37 @@ app.get("/health", () => {
 app.post("/orders", {
   schema: {
     body: z.object({
-       amount: z.number() 
+      amount: z.coerce.number(),
     })
   }
-}, (request, reply) => {
+}, async (request, reply) => {
   const { amount } = request.body
 
   console.log("Creating an order with amount", amount)
 
-  return reply.status(201).send() 
+  const orderId = randomUUID()
+
+  dispatchOrderCreated({
+    orderId,
+    amount,
+    customer: {
+      id: "B9176D35-7276-4255-A323-D825CAEE03B5",
+    },    
+  })
+
+  try {
+    await db.insert(schema.orders).values({
+      id: orderId,
+      customerId: "B9176D35-7276-4255-A323-D825CAEE03B5",
+      amount,
+    })
+  } catch (err) {
+    console.log(err)
+  }
+
+  return reply.status(201).send()
 })
 
-app.listen({ host: "0.0.0.0", port: 3333}).then(() => {
-  console.log("[Orders] HTTP server running!")
+app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
+  console.log("[Orders] HTTP Server running!")
 })
